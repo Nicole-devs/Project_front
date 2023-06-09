9d39488d28a0e7e08cd457de7e7b1d14152e60f7
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-09 18:26:18 +0300

    AddWorkout

---
+++ frontend/src/components/AddWorkout.js
@@ -0,0 +1,121 @@
+import { React, useState } from 'react';
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { InputAdornment } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+
+const defaultValues = {
+  user_id: undefined,
+  title: "",
+  comment: "",
+  star_rating: 0,
+};
+
+function AddWorkout({user}) {
+  const [formValues, setFormValues] = useState(defaultValues);
+  const [reloadPage, setReloadPage] = useState(false)
+  
+  const navigate = useNavigate()
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+      user_id: user.id
+    });
+  };
+  
+  const handleStarHandle = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+      user_id: user.id
+    });
+  }
+
+  console.log(formValues)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(formValues)
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({...formValues}),
+    };
+    fetch(`https://my-workout-space-backend.herokuapp.com/users/${user.id}`, configObj)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+
+    setFormValues(defaultValues);
+    setReloadPage((currentState) => !currentState)
+    window.location.reload(reloadPage)
+  };
+
+  return (
+    <div>
+    <form onSubmit={handleSubmit}>
+    <Box container sx={{ padding: "15px", m:2, border:1, height: "400px", width:"400px", borderRadius: 2 }}>
+        <Grid container alignItems="center" justify="center" direction="column" margin="2%">
+        <Typography sx={{ fontFamily: "Monospace", mb: 1 }} variant="h4" >Add New Workout</Typography>
+          <Grid item marginBottom="1%">
+            <TextField
+            sx={{ mb: 1 }}
+              InputLabelProps={{ shrink: true }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                  </InputAdornment>
+                )
+              }}
+              id="title"
+              name="title"
+              label="Title"
+              type="text"
+              value={formValues.title}
+              onChange={handleChange}
+              required
+              />
+          </Grid>
+            <TextField style={{height: "100%" }}
+            InputLabelProps={{ shrink: true }}
+            InputProps= {{
+              startAdornment: (
+                <InputAdornment position="start">
+                </InputAdornment>
+              )
+            }}
+            id="comment"
+              name="comment"
+              label="Comment"
+              type="comment"
+              value={formValues.comment}
+              onChange={handleChange}
+              />
+          <Box component="fieldset" mb={3} borderColor="transparent">
+            <Typography sx={{ fontFamily: 'Monospace' }}>My Rating:</Typography>
+            <Rating
+              name="star_rating"
+              value={formValues.star_rating}
+              type="number"
+              onChange={handleStarHandle}
+            />
+          </Box>
+          <Button variant="contained" color="secondary" type="submit">Submit</Button>
+        </Grid>
+      </Box>
+    </form>
+    </div>
+  );
+}
+
+export default AddWorkout;
